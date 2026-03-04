8fc853115eeac8799dab066839ea50a86d27fcd7
wulanfawwazia-afk <wulanfawwazia@gmail.com>
2026-03-04 10:28:12 +0700

    Menambahkan HomeController, Routes, dan Views

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,5 @@
+<div>
+    Silakan menghubungi kami di WA:<br>
+    Email: sewadigicam@gmail.com<br>
+    WhatsApp: 08123456789
+</div>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+<div>
+    Selamat Datang di Toko Sewa Digicam Terbaik {{$nama}}<br>
+    Anda adalah seorang {{$pekerjaan}}
+</div>
